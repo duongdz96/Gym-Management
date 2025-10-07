4d8ee7131b1acb8ca2fc8ef1a57a3b52bcade06a
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-10-07 10:51:52 +0700

    config feedback image

---
+++ src/main/java/com/example/gympool/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.gympool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/"); // thư mục cùng cấp với src/
+    }
+}
