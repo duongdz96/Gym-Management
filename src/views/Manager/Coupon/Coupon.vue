a8d791aa071ec4d8c7e1e056ec57a864e135ef10
Ngoc Long <vanngoclong2003@gmail.com>
2025-11-10 17:22:27 +0700

    Update Coupon.vue

--- src/views/Manager/Coupon/Coupon.vue
+++ src/views/Manager/Coupon/Coupon.vue
@@ -1,17 +1,7 @@
 <script setup lang="ts">
 import { computed, ref,onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
-import axios from 'axios'
 import api from '@/services/api';
-
-
-onMounted(async () => {
-    try {
-        const res = await axios.get(`http://localhost:8080/api/product`)
-        console.log(res)
-    } finally {
-    }
-})
 
 type Coupon = {
     id: number,
@@ -122,14 +112,14 @@ function doImport() {
 
                 <select v-model="couponType" class="px-3 py-2 border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 mr-2">
                     <option value="">All type</option>
-                 <option :value="1">Percentage</option>
-                 <option :value="2">Fixed</option>
+                 <option value="PERCENTAGE">Percentage</option>
+                 <option value="FIXED_AMOUNT">Fixed</option>
                 </select>
 
                 <select v-model="couponStatus" class="px-3 py-2 border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800">
                     <option value="">All status</option>
-                 <option :value="1">Active</option>
-                 <option :value="2">Inactive</option>
+                 <option value="ACTIVE">Active</option>
+                 <option value="INACTIVE">Inactive</option>
                 </select>
              </div>
              <div>
@@ -219,9 +209,9 @@ function doImport() {
                         <td class="px-4 py-3 text-sm text-gray-600">{{ formatDate(c.startDate) }} - {{ formatDate(c.endDate) }}</td>
                         <td class="px-4 py-2 text-sm">
                             <span :class="[
-                                'px-2 py-1 rounded-full text-xs', c.status === 1 ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800'
+                                'px-2 py-1 rounded-full text-xs', c.status === 'ACTIVE' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800'
                             ]">
-                            {{ c.status === 1 ? 'Active' : 'Inactive' }}
+                            {{ c.status === 'ACTIVE' ? 'Active' : 'Inactive' }}
                             </span>
                         </td>
                         <td class="px-4 py-3 text-sm text-gray-600">{{ c.scope }}</td>
